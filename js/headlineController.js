cc08d48f2d25f2409eecf30c50bedb80718d4bd9
qingyun <qingyun@zhenzhen.local>
2016-10-12 20:52:18 +0800

    xiugai2

--- js/headlineController.js
+++ js/headlineController.js
@@ -17,7 +17,7 @@ angular.module('myApp.headlineController',[])
     (function () {
         $http({
             method:'jsonp',
-            url:"http://localhost:3001?myUrl="+"http://c.m.163.com/nc/article/headline/T1348647853363/0-20.html"+"&callback=JSON_CALLBACK"
+            url:"http://localhost:3001?myUrl="+"http://c.m.163.com/nc/article/headline/T1348647853363/0-10.html"+"&callback=JSON_CALLBACK"
             
         }).then(function success(result){
             
@@ -25,6 +25,7 @@ angular.module('myApp.headlineController',[])
             //result是数组
             result = result.T1348647853363;
             //删除第一个元素
+            result.splice(2,3);
             result.splice(0,1);
 
             $scope.mine.newsList = result;
